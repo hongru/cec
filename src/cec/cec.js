a948539d227a372be47508f8fb9cacd9d444ee97
hongru <hongru.chenhr@alibaba-inc.com>
2013-11-24 16:22:18 +0800

    add tween

--- src/cec/cec.js
+++ src/cec/cec.js
@@ -1,14 +1,15 @@
 //CEC index
-KISSY.add(function (S, Loader, Sprite, Ticker, Notifier) {
+KISSY.add(function (S, Loader, Sprite, Ticker, Notifier, TWEEN) {
     
     var CEC = {};
     CEC.Loader = Loader;
     CEC.Sprite = Sprite;
     CEC.Ticker = Ticker;
     CEC.Notifier = Notifier;
+    CEC.TWEEN = TWEEN;
 
     return CEC;
 
 }, {
-    requires: ['cec/loader/index', 'cec/sprite/index', 'cec/ticker/index', 'cec/notifier/index'] 
+    requires: ['cec/loader/index', 'cec/sprite/index', 'cec/ticker/index', 'cec/notifier/index', 'cec/tween/tween'] 
 });
